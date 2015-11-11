675b6eba7b6474e3578a9ca7d85857635bb8e1b4
Krstnnnn <k@ioctl.it>
2015-11-11 21:20:03 +0100

    oops.

--- services/paperscraper.nix
+++ services/paperscraper.nix
@@ -1,3 +1,36 @@
+{config, pkgs, lib, ...}:
+
+let
+  cfg = config.services.paperscraper;
+in
+
+with lib;
+
+{
+  options = {
+    services.paperscraper = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = ''
+          Start the PaperScraper API service.
+        '';
+      };
+
+      user = mkOption {
+        default = "username";
+        type = with types; uniq string;
+        description = ''
+          Name of the user to run as.
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    jobs.paperscraper = {
+      description = "Start the paperscraper service.";
+      startOn = "started network-interfaces";
       exec = ''/var/setuid-wrappers/sudo -u ${cfg.user} -- ${pkgs.paperscraper}/bin/PaperScraper'';
     };
 
